d196f79b811ea361d2b479c0043497ce4f60a88f
pipo_ <pipo_@DESKTOP-69PNKN2>
2023-01-13 17:17:31 -0300

    Agregar archivos de proyecto.

---
+++ OracleDBAzureDevOpsAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OracleDBAzureDevOpsAPI.Models;
+
+namespace OracleDBAzureDevOpsAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseOracle(_configuration.GetConnectionString("MiConexion"));
+        }
+
+        public DbSet<Producto> Producto { get; set; }
+    }
+}
---
+++ OracleDBAzureDevOpsAPI/Migrations/20230113193307_MiMigracion.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OracleDBAzureDevOpsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MiMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Producto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Nombre = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Stock = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    Precio = table.Column<double>(type: "BINARY_DOUBLE", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Producto", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Producto");
+        }
+    }
+}
